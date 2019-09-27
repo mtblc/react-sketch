670fb0327c2abff162d9c81922a1f9c52b24c3d4
Sebastián Salata Ruiz-Tagle <sebastian@salatart.com>
2019-09-27 09:57:57 -0300

    feat: add watershed capabilities (#12)

--- src/SketchField.jsx
+++ src/SketchField.jsx
@@ -15,6 +15,9 @@ import Tool from './tools';
 
 const fabric = require('fabric').fabric;
 
+const WATERSHED_OPACITY_FACTOR = 0.75;
+const DEFAULT_BACKGROUND_OPACITY = 1;
+
 /**
  * Sketch Tool based on FabricJS for React Applications
  */
@@ -48,6 +51,8 @@ class SketchField extends PureComponent {
     heightCorrection: PropTypes.number,
     // Specify action on change
     onChange: PropTypes.func,
+    // Specify action on mouse up
+    onMouseUp: PropTypes.func,
     // Default initial value
     defaultValue: PropTypes.object,
     // Sketch width
@@ -65,7 +70,7 @@ class SketchField extends PureComponent {
     lineWidth: 10,
     fillColor: 'transparent',
     backgroundColor: 'transparent',
-    opacity: 1.0,
+    opacity: DEFAULT_BACKGROUND_OPACITY,
     undoSteps: 25,
     tool: Tool.Pencil,
     widthCorrection: 2,
@@ -272,6 +277,13 @@ class SketchField extends PureComponent {
         onChange(e.e)
       }, 10)
     }
+
+    if (this.props.onMouseUp) {
+      let onMouseUp = this.props.onMouseUp;
+      setTimeout(() => {
+        onMouseUp(e.e)
+      }, 10)
+    }
   };
 
   /**
@@ -285,6 +297,7 @@ class SketchField extends PureComponent {
     let { widthCorrection, heightCorrection } = this.props;
     let canvas = this._fc;
     let backgroundCanvas = this._fbc;
+    let watershedCanvas = this._fwc;
     let { offsetWidth, clientHeight } = this._container;
     let prevWidth = canvas.getWidth();
     let prevHeight = canvas.getHeight();
@@ -295,7 +308,7 @@ class SketchField extends PureComponent {
       parentWidth: offsetWidth
     });
 
-    [this._fcursor, canvas, backgroundCanvas].forEach((currentCanvas) => {
+    [this._fcursor, canvas, backgroundCanvas, watershedCanvas].forEach((currentCanvas) => {
       currentCanvas.setWidth(offsetWidth - widthCorrection);
       currentCanvas.setHeight(clientHeight - heightCorrection);
 
@@ -343,7 +356,7 @@ class SketchField extends PureComponent {
    * @param factor the zoom factor
    */
   zoom = (factor) => {
-    [this._fc, this._fbc].forEach((canvas) => {
+    [this._fc, this._fbc, this._fwc].forEach((canvas) => {
       let objects = canvas.getObjects();
       for (let i in objects) {
         objects[i].scaleX = objects[i].scaleX * factor;
@@ -446,7 +459,10 @@ class SketchField extends PureComponent {
    *
    * @returns {String} URL containing a representation of the object in the format specified by options.format
    */
-  toDataURL = (options) => this._fc.toDataURL(options);
+  toDataURL = (options, targetWatershed = false) => {
+    const targetCanvas = targetWatershed ? this._watershedCanvas : this._fc;
+    return targetCanvas.toDataURL(options);
+  }
 
   /**
    * Returns JSON representation of canvas
@@ -487,8 +503,13 @@ class SketchField extends PureComponent {
     let discarded = this.toJSON(propertiesToInclude);
     this._fc.clear();
     this._fbc.clear();
+    this._fwc.clear();
     this._history.clear();
     return discarded
+  };
+
+  clearWatershed = () => {
+    this._fwc.clear();
   };
 
   /**
@@ -578,6 +599,9 @@ class SketchField extends PureComponent {
    */
   setCanvasOpacity = (opacityValue) => {
     this._fc.wrapperEl.style.opacity = opacityValue;
+    this._fwc.wrapperEl.style.opacity = opacityValue === 1
+      ? opacityValue
+      : opacityValue * WATERSHED_OPACITY_FACTOR;
   }
 
   componentDidMount = () => {
@@ -591,12 +615,21 @@ class SketchField extends PureComponent {
 
     let canvas = this._fc = new fabric.Canvas(this._canvas);
     let backgroundCanvas = this._fbc = new fabric.Canvas(this._backgroundCanvas);
+    let watershedCanvas = this._fwc = new fabric.Canvas(this._watershedCanvas);
     const cursorCanvas = this._fcursor = new fabric.StaticCanvas(this._cursor);
     const mouseCursor = new fabric.Circle({ originX: 'center', originY: 'center' });
 
-    // set canvas position
-    canvas.wrapperEl.style.position = 'absolute';
-    backgroundCanvas.wrapperEl.style.position = 'absolute';
+    [canvas, backgroundCanvas, watershedCanvas, cursorCanvas].forEach(c => {
+      c.enableRetinaScaling = false;
+      c.imageSmoothingEnabled = false;
+      c.svgViewportTransformation = false;
+    });
+
+    [canvas, backgroundCanvas, watershedCanvas].forEach(c => {
+      c.wrapperEl.style.position = 'absolute';
+    });
+
+    watershedCanvas.wrapperEl.style.opacity = DEFAULT_BACKGROUND_OPACITY * WATERSHED_OPACITY_FACTOR;
     cursorCanvas.lowerCanvasEl.style.position = 'absolute';
     cursorCanvas.lowerCanvasEl.style.pointerEvents = 'none';
 
@@ -692,6 +725,9 @@ class SketchField extends PureComponent {
           id={uuid4()}
           ref={(c) => this._backgroundCanvas = c}
         >
+          Sorry, Canvas HTML5 element is not supported by your browser
+        </canvas>
+        <canvas id="watershed" ref={(c) => this._watershedCanvas = c}>
           Sorry, Canvas HTML5 element is not supported by your browser
         </canvas>
         <canvas
